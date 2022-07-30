a6386bcc84c259ac16a516f8de4700e4bd14b41a
vicmuchina <vicmuchina1234@gmail.com>
2022-07-31 00:58:06 +0300

    finding prime number algo added

--- hello.py
+++ hello.py
@@ -35,4 +35,12 @@ for i in range(len(h)):
 
 for i in range(0,100,3):
     print(i)
-    
+
+for n in range(2,100):
+    for x in range(2,n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+         # loop fell through without finding a factor
+        print(n, 'is a prime number')
